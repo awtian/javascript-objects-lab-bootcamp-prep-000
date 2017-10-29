7c0ffb9dd4a4f5e3371aa337cc6c490a20db8f0e
Awtian Akbar <me@awtian.com>
2017-10-29 12:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue (obj, key, val) {
   obj[key] = val
   return obj
 }
+
+function deleteFromObjectByKey (obj, key) {
+  var temp = obj
+  delete temp[key]
+  return temp
+}
